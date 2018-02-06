a328e5de157b56a96ec73517d50d448d61aef178
David Becker <david@beckersite.com>
2018-02-06 16:33:54 -0700

    Finish test seed files

--- db/seeds/test/posts-test.js
+++ db/seeds/test/posts-test.js
@@ -1,13 +1,15 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+exports.seed = (knex, Promise) => {
+  return knex('comments').del()
+    .then(() => {
+      return knex('comments').insert([
+        {id: 1, author: 'author2', body: 'comment1', post_id: 1},
+        {id: 2, author: 'author1', body: 'comment2', post_id: 4},
+        {id: 3, author: 'author3', body: 'comment3', post_id: 4},
+        {id: 4, author: 'author2', body: 'comment4', post_id: 5},
       ]);
-    });
+    })
+    .then(() => {
+			console.log('Seeding is complete.');
+		})
+		.catch(error => console.log(`Error seeding data: ${error}`));
 };
