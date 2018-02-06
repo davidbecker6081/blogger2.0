3d400edd32da92916ad7ec56999dfa597d3eeb58
David Becker <david@beckersite.com>
2018-02-06 16:17:53 -0700

    Finish table schema for posts and comments

--- db/migrations/20180201195016_initial.js
+++ db/migrations/20180201195016_initial.js
@@ -1,7 +1,30 @@
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('posts', table => {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('body');
+      table.string('author');
+      table.integer('#_of_comments');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('comments', table => {
+      table.increments('id').primary();
+      table.string('author');
+      table.string('body');
+      table.integer('post_id').unsigned();
+      table.foreign('post_id').references('posts.id').onDelete('cascade');
+
+      table.timestamps(true, true);
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-
+  return Promise.all([
+    knex.schema.dropTable('comments'),
+    knex.schema.dropTable('posts')
+  ])
 };
